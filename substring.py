072fb48c295ca58f8dc8300c693e740ddb2eaf81
adithya14255 <adithya14255@gmail.com>
2024-09-05 23:45:48 +0530

    update

---
+++ substring.py
@@ -0,0 +1,24 @@
+def count_substring(string, sub_string):
+    count=0
+    temp=0
+    stridx=0
+    while stridx<len(string):
+        if sub_string[temp]==string[stridx]:
+            stridx+=1
+            temp+=1
+            if temp==len(sub_string):
+                count+=1
+                stridx-=temp-1
+                temp=0
+        else:
+            stridx+=1
+            temp=0
+    return count
+        
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
