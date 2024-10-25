93d655baa72469ea477117f68e8d3c346fd3beea
Adithya G <112386210+Adithya14255@users.noreply.github.com>
2024-10-26 00:19:35 +0530

    Update validParenthesis.py

--- validParenthesis.py
+++ validParenthesis.py
@@ -1,22 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        i=0
         opening_stuff = ['(','[','{']
         closing_stuff = [')',']','}']
         temp_arr = []
-        while(i< len(s)):
-            if s[i] in opening_stuff:
-                temp_arr.append(s[i])
-                i+=1
-            elif s[i] in closing_stuff and len(temp_arr):
-                if opening_stuff.index(temp_arr[-1])==closing_stuff.index(s[i]):
+        for i in s:
+            if i in opening_stuff:
+                temp_arr.append(i)
+            elif i in closing_stuff and len(temp_arr):
+                if opening_stuff.index(temp_arr[-1])==closing_stuff.index(i):
                     temp_arr.pop(-1)
-                    i+=1
                 else:
                     return False
             else:
                 return False
-        if not len(temp_arr):
-            return True 
-        else:
-            return False
+        return False if len(temp_arr) else True
